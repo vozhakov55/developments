62407ac9f44b91f3e27006f1537e520a1fe6e31e
vozhakov55 <44494865+vozhakov55@users.noreply.github.com>
2018-10-26 21:04:04 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer>
+  <div class="container footer1">
+      
+   <div class="row">
+      <div class="col"> <i class="fa fa-phone" aria-hidden="true" style="color: #009ED6;"></i> Контактный телефон: 
+          +9989353229917<br>
+      <i class="fa fa-envelope" aria-hidden="true" style="color: #009ED6;"></i> E-mail: kurausan.@mail.ru
+     </div>
+        <div class="col-7">
+         <br><i class="fa fa-copyright copyright" aria-hidden="true" style="color: #009ED6;"></i> 2018 http://vokura.ru
+        </div>
+  </div>
+
+  </div>
+</footer>
+                
+    </div> <!-- конец  <div class="container" id="main"> -->
+</div> <!--конец обертки -->
+
+  <!-- Подключаем мой скрипт  
+    <script type="text/javascript" src="js/myScript.js"></script    
+-->
+
+    <!-- Подключаем jQuery -->
+    <script type="text/javascript" src="jquery/jquery-3.3.1.min.js"></script>   
+
+
+    <!-- Подключаем плагин Popper (необходим для работы компонента Dropdown и др.)  
+    <script type="text/javascript" src="js/popper.min.js"></script>   
+   -->
+
+    <!-- Подключаем Bootstrap JS  -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+   
+</body>
+</html>
